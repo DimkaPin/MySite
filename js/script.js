2b3a3f5c6ec016d302e5026622ec4c84f829cada
unknown <pinyaev_1997@mail.ru>
2018-09-28 17:11:00 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var i, lg, fedya, pekka, total;
+lg = 10;
+fedya = 2; 
+total = 1;
+switch (lg+fedya){
+     case 10: {
+       for (i = 0; i <= 5; i++) {
+         pekka=i%2;
+         if (pekka!=0)
+         total = total+i;
+       console.log (i);
+       console.log (total);
+          break;
+              }
+            }
+     case 12: {
+       var MyAir = new Object(); {
+        MyAir .name = "Boein" ; 
+       MyAir .model = "777";
+       MyAir .year = "2019";
+       MyAir .color = "blue";
+          break;
+              }
+       }
+     case 14: {
+       console.log ("четырнадцать");
+          break;
+              }
+     default: {
+       console.log ("не знаю");
+          break;
+              }
+              }
