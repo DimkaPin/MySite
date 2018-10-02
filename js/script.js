ded1de940a1acb5254c506214e908f23ec5b1248
DimkaPin <pinyaev_1997@mail.ru>
2018-10-02 13:38:41 +0300

    my BMW

--- js/script.js
+++ js/script.js
@@ -1,43 +1,28 @@
-/*
-var i, lg, fedya, pekka, total;
-lg = 10;
-fedya = 2; 
-total = 1;
-switch (lg+fedya){
-     case 10: {
-       for (i = 0; i <= 5; i++) {
-         pekka=i%2;
-         if (pekka!=0)
-         total = total+i;
-       console.log (i);
-       console.log (total);
-          break;
-              }
-            }
-     case 12: {
-       var MyAir = new Object(); {
-        MyAir .name = "Boein" ; 
-       MyAir .model = "777";
-       MyAir .year = "2019";
-       MyAir .color = "blue";
-          break;
-              }
-       }
-     case 14: {
-       console.log ("четырнадцать");
-          break;
-              }
-     default: {
-       console.log ("не знаю");
-          break;
-              }
-              }
-              */
+var a, b, c, sum, raz, pro, de2, slovo1, slovo2, slovo3, slovo4, sl1, sl2;
 
-var a = 1;
- var b = 0;
-while (a<=10) {
-b=b+a
-a++;
-}
-console.log (b);
+a = 100;
+b = 25;
+c = 36;
+sum = a+b+c;
+console.log(sum);
+raz = b-a-c;
+console.log(raz);
+pro = a*b*c;
+console.log(pro);
+de2= b/a/c;
+console.log(de2);
+
+
+console.log("---Slova---");
+slovo1 = "Dimka";
+slovo2 = "Dashka";
+slovo3 = "Tolya";
+slovo4 = "Katya";
+sl1 = slovo1+ "," +slovo2;
+console.log(sl1);
+sl2 = slovo3+ "," +slovo4;
+console.log(sl2);
+
+
+
+
